8517c609bf1c9ed9d9f48bfd48053c4706426d43
Banele Makhanya <bmakhanya424@gmail.com>
2026-06-15 17:11:13 +0200

    new

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import { organizationsPage, showOrganizationDetailsPage, showNewOrganizationForm
 import { projectsPage,showProjectDetailsPage, showProjectsPage, showNewProjectForm, processNewProjectForm, projectValidation, showEditProjectForm, processEditProjectForm } from './controllers/projects.js';
 import { categoriesPage, showCategoryDetailsPage, showAssignCategoriesForm, processAssignCategoriesForm, showNewCategoryForm, processNewCategoryForm, showEditCategoryForm, processEditCategoryForm, categoryValidation } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
-import { showUserRegistrationForm, processUserRegistrationForm } from './controllers/users.js';
+import { showUserRegistrationForm, processUserRegistrationForm, showLoginForm, processLoginForm, processLogout } from './controllers/users.js';
 
 
 
@@ -34,7 +34,9 @@ router.get('/edit-category/:id', showEditCategoryForm);
 router.post('/edit-category/:id', categoryValidation, processEditCategoryForm);
 router.get('/register', showUserRegistrationForm);
 router.post('/register', processUserRegistrationForm);
-
+router.get('/login', showLoginForm);
+router.post('/login', processLoginForm);
+router.get('/logout', processLogout);
 
 
 // error-handling routes
